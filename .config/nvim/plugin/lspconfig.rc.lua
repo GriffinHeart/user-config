f2ff01a986607f7d374e61bd4c7cb204f21239e9
Hugo Almeida <hugofalmeida@gmail.com>
2023-01-31 16:22:22 +0900

    Add diagnostic keymaps; retab

--- .config/nvim/plugin/lspconfig.rc.lua
+++ .config/nvim/plugin/lspconfig.rc.lua
@@ -6,53 +6,56 @@ local protocol = require('vim.lsp.protocol')
 local on_attach = function()
 end
 -- local on_attach = function(client, bufnr)
--- 	-- formatting
--- 	if client.server_capabilities.documentFormattingProvider then
--- 		vim.api.nvim_command [[augroup Format]]
--- 		vim.api.nvim_command [[autocmd! * <buffer>]]
--- 		vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
--- 		vim.api.nvim_command [[augroup END]]
--- 	end
+--  -- formatting
+--  if client.server_capabilities.documentFormattingProvider then
+--    vim.api.nvim_command [[augroup Format]]
+--    vim.api.nvim_command [[autocmd! * <buffer>]]
+--    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+--    vim.api.nvim_command [[augroup END]]
+--  end
 -- end
 
 -- brew install lua-language-server
 nvim_lsp.sumneko_lua.setup {
-	on_attach = on_attach,
-	settings = {
-		Lua = {
-			diagnostics = {
-				-- recognize the vim global
-				globals = { 'vim' }
-			},
-			workspace = {
-				-- Let the server know about Neovim runtime files
-				library = vim.api.nvim_get_runtime_file('', true)
-			}
-		}
-	}
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- recognize the vim global
+        globals = { 'vim' }
+      },
+      workspace = {
+        -- Let the server know about Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true)
+      }
+    }
+  }
 }
 
 -- pipx install python-lsp-server[all]
 -- user pipx inject python-lsp-server <plugins> to install additional plugins
 -- TODO: Revisit these configs after setup
 nvim_lsp.pylsp.setup {
-	on_attach = on_attach,
-	settings = {
-		pylsp = {
-			plugins = {
-				-- TODO: check the other tools configs
-				flake8 = {
-					enabled = true,
-				},
-				black = { enabled = true, },
-				pycodestyle = {
-					enabled = false,
-					ignore = { 'W391' },
-					maxLineLength = 100
-				},
-			}
-		}
-	}
+  on_attach = on_attach,
+  settings = {
+    pylsp = {
+      plugins = {
+        -- TODO: check the other tools configs
+        flake8 = {
+          enabled = true,
+        },
+        pylint = {
+          enabled = true,
+        },
+        black = { enabled = true, },
+        pycodestyle = {
+          enabled = false,
+          ignore = { 'W391' },
+          maxLineLength = 100
+        },
+      }
+    }
+  }
 }
 
 -- install elixir-ls in ~/elixir-ls
